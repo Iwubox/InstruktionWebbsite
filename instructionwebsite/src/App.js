abf1b8ed7fb484b33241cf3c615d660e966626f6
Iwubox <arvin@framecore.se>
2025-05-07 17:27:48 +0200

    nav + resposive commit

--- instructionwebsite/src/App.js
+++ instructionwebsite/src/App.js
@@ -1,6 +1,27 @@
 import "./App.css";
+import { useState } from "react";
+
+//imges
+import hamburgerPNG from "./resorces/hambugerMenu.png";
 
 function App() {
+  let flipflop = true;
+  let hamburgerMenu = () => {
+    let hamburger = document.getElementsByClassName("hamburgerMenu")[0];
+
+    if (flipflop) {
+      hamburger.style.rotate = "90deg";
+      hamburger.style.transition = "0.5s";
+      flipflop = false;
+    } else if (flipflop == false) {
+      hamburger.style.rotate = "0deg";
+      hamburger.style.transition = "0.5s";
+      flipflop = true;
+    } else {
+      alert("Error: flipflop ");
+    }
+  };
+
   return (
     <div className="app">
       <div className="navbar">
@@ -17,8 +38,8 @@ function App() {
           <div className="navItem">
             <p>Contact</p>
           </div>
-          <div className="hamburger">
-            <img></img>
+          <div className="hamburgerMenu" onClick={hamburgerMenu}>
+            <img src={hamburgerPNG} alt="hamburger menu"></img>
           </div>
         </div>
       </div>
