06e77dbf6c1bcb44af2212269278ee729cf59664
arvin <arvin@framecore.se>
2025-05-18 01:57:57 +0200

    fixing hambergur dropdown

--- instructionwebsite/src/App.js
+++ instructionwebsite/src/App.js
@@ -8,14 +8,23 @@ function App() {
   let flipflop = true;
   let hamburgerMenu = () => {
     let hamburger = document.getElementsByClassName("hamburgerMenu")[0];
+    let hamburgerList = document.getElementsByClassName("hamburgerList")[0];
 
     if (flipflop) {
       hamburger.style.rotate = "90deg";
       hamburger.style.transition = "0.5s";
+
+      hamburgerList.style.display = "flex";
+      hamburgerList.style.rotate = "0deg";
+
+
       flipflop = false;
     } else if (flipflop == false) {
       hamburger.style.rotate = "0deg";
       hamburger.style.transition = "0.5s";
+
+      hamburgerList.style.display = "none";
+
       flipflop = true;
     } else {
       alert("Error: flipflop ");
@@ -41,15 +50,26 @@ function App() {
           <div className="hamburgerMenu" onClick={hamburgerMenu}>
             <img src={hamburgerPNG} alt="hamburger menu"></img>
           </div>
+          <div className="hamburgerList">
+              <div className="hambItem">
+                <p>Home</p>
+              </div>
+              <div className="hambItem">
+                <p>About</p>
+              </div>
+              < div className="HambItem">
+                <p>Contact</p>
+              </div>
+            </div>
         </div>
       </div>
+      <p>wwww</p>
       <p>hdhwa</p>
       <p>hdhwa</p>
       <p>hdhwa</p>
       <p>hdhwa</p>
       <p>hdhwa</p>
-      <p>hdhwa</p>
-      <p>hdhwa</p>
+      <p>wwww</p>
       <p>hdhwa</p>
       <p>hdhwa</p>
       <p>hdhwa</p>
