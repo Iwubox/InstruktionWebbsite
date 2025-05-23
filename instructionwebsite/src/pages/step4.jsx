facab4f8d15478229b419b3c0d51173d5f9952b2
Iwubox <arvin@framecore.se>
2025-05-23 11:06:36 +0200

    instructions text

--- instructionwebsite/src/pages/step4.jsx
+++ instructionwebsite/src/pages/step4.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import { motion } from "motion/react";
 
 const Step4 = () => {
-  return <body></body>;
+  return <body>
+
+  <p>Step 4</p>
+  <p>Install and setup coding enviroment</p>
+
+  </body>;
 };
 
 export default Step4;
--- instructionwebsite/src/pages/step5.jsx
+++ instructionwebsite/src/pages/step5.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import { motion } from "motion/react";
 
 const Step5 = () => {
-  return <body></body>;
+  return <body>
+
+    <p>Step 5</p>
+    <p>Getting api key</p>
+
+    <p> The core component that will allow the chatbot to work is the api key. This will be grabbed from OpenAIs webbsite and will allow you to make calls up to 5 dollars worth of calls. After this you wont be able to use your chatbot for free</p>
+    <p>Now go to OpenAIs </p>
+  </body>;
 };
 
 export default Step5;
